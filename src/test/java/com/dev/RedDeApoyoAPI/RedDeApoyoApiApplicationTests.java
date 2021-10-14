938aa1154b5d63f15fc6d7f1c01c46a1ab948814
zulyParra <zulyatrianap@gmail.com>
2021-10-14 11:21:43 -0500

    proyecto inicial

---
+++ src/test/java/com/dev/RedDeApoyoAPI/RedDeApoyoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.RedDeApoyoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedDeApoyoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
